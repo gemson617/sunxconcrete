0d6d22505b4da2e1907d0c2bc82b8e5179e07204
Gemson <gemson617@gmail.com>
2023-11-14 15:15:33 +0530

    changes

--- application/controllers/DcController.php
+++ application/controllers/DcController.php
@@ -15,7 +15,6 @@ class DcController extends MY_Controller
 
     public function view()
     {
-       
         $this->db->select('s.*, s.id AS id,c.company_name,
                           SUM(sSub.total_qty) as totalQuantity,');
         $this->db->from('sales_order as s');   
@@ -24,8 +23,27 @@ class DcController extends MY_Controller
         $this->db->order_by('s.id','DESC');  
         $this->db->group_by('sSub.sales_order_id','DESC');
         $query = $this->db->get()->result();
-
         $view_data['sale'] = $query;
+        $view_data['sale'] ['transaction_id'] = [];
+
+        foreach($view_data['sale']  as $key=> $row){
+
+            $this->db->select('*');
+            $this->db->from('sales_order_items as s'); 
+            $this->db->where('s.sales_order_id',$row->id); 
+            $this->db->group_by('s.transaction_id');  
+            $query_items = $this->db->get()->result();
+            $view_data['sale'][$key]->saless = $query_items;
+
+            // Array to store transaction IDs for each sale
+            $transaction_ids = [];
+
+            foreach ($view_data['sale'][$key]->saless as $item_key => $item) {
+                $transaction_ids[] = $item->transaction_id;
+            }
+            // Assign the array of transaction IDs to the sale data
+            $view_data['sale'][$key]->transaction_id = $transaction_ids;
+        }
 
         $data = array(
             'title' => 'Sales Orders',
